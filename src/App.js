76a59e19cd17f0d85363f294557bed20f671d687
Miqueias Fernandes <miqueiasfernandes@bitbucket.com>
2020-03-08 17:08:12 -0300

    Created part of todo list page layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
+import { Container, Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavItem, NavLink, Row, Col, Input, Button } from 'reactstrap'
 
 function App() {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container>
+        <Navbar color="dark" dark expand="md">
+          <NavbarToggler onClick={toggle}/>
+          <NavbarBrand href="#">ToDo App</NavbarBrand>
+          <Collapse isOpen={!isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+
+              {/* ToDo List Page */}
+              <NavItem>
+                <NavLink href="#">
+                Todo List
+                </NavLink>
+              </NavItem>
+
+              {/* About Us Page */}
+              <NavItem>
+                <NavLink href="#">
+                  About
+                </NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+        <main>
+        <Row className="mt-5 pb-3" xs="12">
+          <Col>
+            <h1 className="d-inline">Tasks</h1><h2 className="descr d-inline ml-2">Register</h2>
+            <hr/>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs="9">
+            <Input xs="9" type="text" placeholder="Add your task here..." name="taskTxt" id="taskTxt"/>
+          </Col>
+          <Col xs="3">
+            <Button className="btnAdd" color="info" >
+              Add
+            </Button>
+            <Button className="ml-left" outline color="secoundary" >
+              Search
+            </Button>
+          </Col>
+        </Row>
+        </main>
+      </Container>
     </div>
   );
 }
